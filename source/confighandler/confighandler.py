12cea0a6d28caeade1997298bcfd2c7d02e04a92
LouisPauchet <louis.pauchet@insa-rouen.fr>
2024-11-19 10:57:01 +0100

    Adding skeleton for confighandler.py

---
+++ source/confighandler/confighandler.py
@@ -0,0 +1,24 @@
+config_files = [
+    'static/config/fixed_stations.json',
+    'static/config/mobile_stations.json'
+]
+
+class ConfigHandler:
+    def __init__(self):
+        self.config_files = config_files
+
+    def get_variable(self, station_id):
+
+        pass
+
+    def get_metadata(self, station_id):
+
+        pass
+
+    def get_stations(self, type):
+    
+        pass
+
+    def _load_config(self):
+
+        pass
