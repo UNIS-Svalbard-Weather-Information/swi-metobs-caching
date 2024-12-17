fd67e197d248553ca4b7743b5738cbb4e4210377
LouisPauchet <louis.pauchet@insa-rouen.fr>
2024-12-17 12:44:33 +0100

    Update config docs

--- docs/conf.py
+++ docs/conf.py
@@ -1,44 +1,55 @@
 import os
 import sys
+import subprocess
 import sphinx_rtd_theme
 
-sys.path.insert(0, os.path.abspath('..'))
+# Add the project source directory to the path
+sys.path.insert(0, os.path.abspath('../source'))
 
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+# Run sphinx-apidoc to regenerate .rst files for ./source/
+def run_apidoc(_):
+    """Generate API documentation using sphinx-apidoc."""
+    source_dir = os.path.abspath('../source')
+    output_dir = os.path.abspath('./source')  # Where .rst files will be stored
+    exclude_patterns = ['tests', '*/tests/*']  # Exclude test directories
+    cmd = [
+        'sphinx-apidoc',
+        '--force',            # Overwrite existing files
+        '--module-first',     # Place module docstring before submodule docs
+        '-o', output_dir,     # Output directory
+        source_dir            # Source code directory
+    ]
+    subprocess.call(cmd)
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+# Hook into Sphinx build lifecycle
+def setup(app):
+    app.connect('builder-inited', run_apidoc)
 
+# -- Project Information -----------------------------------------------------
 project = 'SWI - Svalbard Weather Information'
 copyright = '2024, Louis Pauchet (UNIS - INSA Rouen) & Contributors'
 author = 'Louis Pauchet (UNIS - INSA Rouen) & Contributors'
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
-# -- General configuration ---------------------------------------------------
+# -- General Configuration ---------------------------------------------------
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.todo',
+    'sphinx.ext.autodoc',      # Auto-generate documentation from docstrings
+    'sphinx.ext.autosummary',  # Generate summary tables for modules
+    'sphinx.ext.napoleon',     # Support for Google/NumPy style docstrings
+    'sphinx.ext.viewcode',     # Link to source code
 ]
 
-# Napoleon settings for Google-style docstrings
+# Napoleon settings
 napoleon_google_docstring = True
 napoleon_numpy_docstring = False
 
-# Paths
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '**.ipynb_checkpoints', './docs/_build']
+# Autodoc settings
+autosummary_generate = True
+autodoc_default_options = {
+    'members': True,
+    'undoc-members': True,
+    'show-inheritance': True,
+}
 
 # Theme
 html_theme = 'sphinx_rtd_theme'
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-#html_static_path = ['./docs/_static']
-
-# Master document
-master_doc = 'index'
