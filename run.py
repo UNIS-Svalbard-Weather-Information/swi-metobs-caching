1b3b3d3390aff962d6b85e002cb0c6a8e53bb2ad
LouisPauchet <louis.pauchet@insa-rouen.fr>
2025-02-06 12:45:42 +0100

    update run file

--- run.py
+++ run.py
@@ -1,8 +1,15 @@
-from flask import Flask
-from source.app.app import create_app
+import os
+import sys
 
-# Initialize the Flask application
-app = create_app()
+# Ensure the application directory is in the Python path
+sys.path.insert(0, os.path.dirname(__file__))
+
+from source.app.app import create_app  # Import your Flask app factory function
+
+# Create the Flask application instance
+application = create_app()
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    application.run(host='127.0.0.1', port=5000, debug=True)
+
+
