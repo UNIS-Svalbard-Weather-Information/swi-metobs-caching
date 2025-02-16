e7c2249d89284bb120e6e65c5f6b387aff21f079
LouisPauchet <louis.pauchet@insa-rouen.fr>
2025-02-16 01:38:00 +0100

    Update maps processing __init__.py

--- source/maps_processing/__init__.py
+++ source/maps_processing/__init__.py
@@ -1,3 +1,4 @@
 """Maps processing module initialization."""
 from .sea_ice_map_processing import *
 from .avalanche_forecast_processing import AvalancheForecastProcessing
+from .maps_caching import MapsCaching
