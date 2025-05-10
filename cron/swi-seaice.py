caffda32c2f764ab703754dcac31138ff27aa3e5
LouisPauchet <louis.pauchet@insa-rouen.fr>
2025-05-10 11:55:24 +0200

    add retry for cron sea ice

--- cron/swi-seaice.py
+++ cron/swi-seaice.py
@@ -1,5 +1,14 @@
 from source.maps_processing.sea_ice_map_processing import SeaIceCache
 
-sea_ice_handler = SeaIceCache()
+max_try = 10
+i=1
 
-sea_ice_handler.create_ice_chart_geojson()
+while i < max_try:
+    print(f"Try {i}/{max_try}")
+    try:
+        sea_ice_handler = SeaIceCache()
+
+        sea_ice_handler.create_ice_chart_geojson()
+        i= max_try+1
+    except:
+        i = i+1
