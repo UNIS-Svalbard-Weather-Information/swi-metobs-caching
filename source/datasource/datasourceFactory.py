ab939650b7d88d4f9c5406efb5ed2a942c9ea117
LouisPauchet <louis.pauchet@insa-rouen.fr>
2025-06-11 22:58:35 +0200

    addind datasource config and creating sample datasource file + config datasource factory

--- source/datasource/datasourceFactory.py
+++ source/datasource/datasourceFactory.py
@@ -8,12 +8,14 @@ from source.configHandler.confighandler import ConfigHandler
 from source.datasource.FrostSource import FrostSource
 from source.datasource.IWINFixedSource import IWINFixedSource
 from source.datasource.FrostBoatSource import FrostBoatSource
+from source.datasource.HolfuySource import HolfuySource
 
 # Mapping dictionary for supported data sources
 DATASOURCE_MAPPING = {
     "FrostSource": FrostSource,
     "FrostBoatSource" : FrostBoatSource,
-    "IWINFixedSource": IWINFixedSource
+    "IWINFixedSource": IWINFixedSource,
+    "HolfuySource" : HolfuySource
 }
 
 
